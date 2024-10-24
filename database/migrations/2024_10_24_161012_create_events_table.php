2469ef9ec2e31443b0f0426e4e811e3851b07bf7
francescofalcone24 <cicciofalcon97@gmail.com>
2024-10-24 19:57:47 +0300

    template ready

---
+++ database/migrations/2024_10_24_161012_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('location');
+            $table->string('user_email');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->string('start_hour');
+            $table->string('end_hour');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
